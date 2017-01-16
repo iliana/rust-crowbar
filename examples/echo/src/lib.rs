f2d0445240fdd8abcd53dd3e70d0954b51037f22
Ian Weller <ianweller@buttslol.net>
2017-01-16 13:02:28 +0000

    Show off using lambda! with a closure

--- examples/echo/src/lib.rs
+++ examples/echo/src/lib.rs
@@ -3,14 +3,10 @@ extern crate crowbar;
 #[macro_use]
 extern crate cpython;
 
-use crowbar::{Value, LambdaContext, LambdaResult};
-
-fn echo(event: Value, context: LambdaContext) -> LambdaResult {
+lambda!(|event, context| {
     println!("hello cloudwatch logs from {} version {}, {} ms remaining",
              context.function_name(),
              context.function_version(),
              context.get_remaining_time_in_millis()?);
     Ok(event)
-}
-
-lambda!(echo);
+});
