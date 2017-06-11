d2ed8b1dcb6a6613d4b67a9ea3ef86bf38762a3c
Iliana Weller <ilianaw@buttslol.net>
2017-06-11 16:44:54 -0700

    Use pyenv in crowbar-builder to support Python 3.6

--- builder/build.sh
+++ builder/build.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 set -euo pipefail
 export CARGO_TARGET_DIR=$(mktemp -d)
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
 
 (
-if [[ $# -gt 0 ]]; then
-    yum install -y "$@"
-fi
-. $HOME/.cargo/env
-cargo build --release
+    if [[ $# -gt 0 ]]; then
+        yum install -y "$@"
+    fi
+    . $HOME/.cargo/env
+    cargo build --release
 ) 1>&2
 cd $CARGO_TARGET_DIR/release
 (
-strip liblambda.so
-zip lambda.zip liblambda.so
+    strip liblambda.so
+    zip lambda.zip liblambda.so
 ) 1>&2
 exec cat lambda.zip
