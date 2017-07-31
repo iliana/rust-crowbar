12b303a04ab5989a6b38bbf6fba34da31f493ea8
arcnmx <arcnmx@users.noreply.github.com>
2017-07-31 01:15:22 -0400

    doc typo

--- src/lib.rs
+++ src/lib.rs
@@ -238,7 +238,7 @@ pub fn handler<F>(py: Python, f: F, py_event: PyObject, py_context: PyObject) ->
 /// Macro to wrap a Lambda function handler.
 ///
 /// Lambda functions accept two arguments (the event, a serde_json `Value`, and the context, a
-/// `LambdaContext`) and returns a vluae (a serde_json `Value`). The function signature should look
+/// `LambdaContext`) and returns a value (a serde_json `Value`). The function signature should look
 /// like:
 ///
 /// ```rust,ignore
