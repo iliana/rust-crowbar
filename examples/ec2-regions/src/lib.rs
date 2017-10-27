b4cd2156f32739561024b924a00ccfc49c68495b
Iliana Weller <ilianaw@buttslol.net>
2017-10-26 23:25:12 -0700

    rustfmt examples

--- examples/ec2-regions/src/lib.rs
+++ examples/ec2-regions/src/lib.rs
@@ -1,13 +1,13 @@
-#[macro_use(lambda)]
-extern crate crowbar;
 #[macro_use]
 extern crate cpython;
+#[macro_use(lambda)]
+extern crate crowbar;
 extern crate rusoto_core;
 extern crate rusoto_ec2;
 
-use crowbar::{Value, LambdaContext, LambdaResult};
-use rusoto_core::{DefaultCredentialsProvider, Region, default_tls_client};
-use rusoto_ec2::{Ec2, Ec2Client, DescribeRegionsRequest};
+use crowbar::{LambdaContext, LambdaResult, Value};
+use rusoto_core::{default_tls_client, DefaultCredentialsProvider, Region};
+use rusoto_ec2::{DescribeRegionsRequest, Ec2, Ec2Client};
 use std::default::Default;
 use std::env;
 use std::str::FromStr;
@@ -15,9 +15,11 @@ use std::str::FromStr;
 fn list_regions(_: Value, _: LambdaContext) -> LambdaResult {
     let provider = DefaultCredentialsProvider::new()?;
     let region_str = env::var("AWS_DEFAULT_REGION")?;
-    let client = Ec2Client::new(default_tls_client()?,
-                                provider,
-                                Region::from_str(&region_str)?);
+    let client = Ec2Client::new(
+        default_tls_client()?,
+        provider,
+        Region::from_str(&region_str)?,
+    );
     let input: DescribeRegionsRequest = Default::default();
 
     match client.describe_regions(&input)?.regions {
