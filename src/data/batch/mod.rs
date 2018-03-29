f6588595806fe21e4496d562454b9335e8f08b40
Naftuli Kay <me@naftuli.wtf>
2018-03-29 11:19:28 -0700

    passing tests for cloudwatch batch events

--- src/data/batch/mod.rs
+++ src/data/batch/mod.rs
@@ -25,9 +25,9 @@ pub struct JobDetail {
     pub job_queue: String,
     pub status: JobStatus,
     pub attempts: Vec<JobAttempt>,
-    pub created_at: Option<DateTime<Utc>>,
-    pub started_at: Option<DateTime<Utc>>,
-    pub stopped_at: Option<DateTime<Utc>>,
+    pub created_at: Option<u64>,
+    pub started_at: Option<u64>,
+    pub stopped_at: Option<u64>,
     pub retry_strategy: RetryStrategy,
     pub depends_on: Vec<JobDependency>,
     pub job_definition: String,
@@ -39,8 +39,8 @@ pub struct JobDetail {
 #[serde(rename_all="camelCase")]
 pub struct JobAttempt {
     pub container: JobAttemptContainer,
-    pub started_at: DateTime<Utc>,
-    pub stopped_at: DateTime<Utc>,
+    pub started_at: u64,
+    pub stopped_at: u64,
     pub status_reason: String,
 }
 
@@ -70,16 +70,17 @@ pub struct RetryStrategy {
 #[derive(Serialize,Deserialize)]
 #[serde(rename_all="camelCase")]
 pub struct ContainerInfo {
-    pub command: Option<String>,
-    pub container_instance_arn: String,
+    pub command: Vec<String>,
+    pub container_instance_arn: Option<String>,
     pub environment: Vec<BTreeMap<String, String>>,
-    pub exit_code: u16,
+    pub exit_code: Option<u16>,
     pub image: String,
-    pub job_role_arn: String,
-    pub log_stream_name: String,
+    pub job_role_arn: Option<String>,
+    pub log_stream_name: Option<String>,
     pub memory: u64,
     pub mount_points: Vec<ContainerMountPoint>,
     pub privileged: bool,
+    #[serde(rename="readonlyRootFilesystem")]
     pub read_only_root_filesystem: bool,
     pub reason: String,
     pub task_arn: String,
@@ -90,6 +91,7 @@ pub struct ContainerInfo {
 }
 
 #[derive(Serialize,Deserialize)]
+#[serde(rename_all="camelCase")]
 pub struct Ulimit {
     pub hard_limit: u64,
     pub soft_limit: u64,
@@ -109,13 +111,14 @@ pub struct ContainerVolumeHost {
 }
 
 #[derive(Serialize,Deserialize)]
+#[serde(rename_all="camelCase")]
 pub struct ContainerMountPoint {
     pub container_path: String,
     pub read_only: bool,
     pub source_volume: String,
 }
 
-#[derive(Serialize,Deserialize)]
+#[derive(Debug,Eq,PartialEq,Serialize,Deserialize)]
 #[serde(rename_all="SCREAMING_SNAKE_CASE")]
 pub enum JobStatus {
     Failed,
@@ -124,5 +127,5 @@ pub enum JobStatus {
     Running,
     Starting,
     Submitted,
-    Suceeded,
+    Succeeded,
 }
